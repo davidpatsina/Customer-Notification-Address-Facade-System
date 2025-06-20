04c6e62182ff42bb7a5120049a30612a1996a811
davidpatsina <dpats19@freeuni.edu.ge>
2025-06-20 18:12:42 +0400

    Initial commit

---
+++ src/main/java/com/example/cnafs/CnafsApplication.java
@@ -0,0 +1,13 @@
+package com.example.cnafs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnafsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnafsApplication.class, args);
+	}
+
+}
